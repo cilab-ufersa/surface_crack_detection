8b44d83d64cafd389284ca70a575181e8e81cadf
Rosana <rosanacibely@gmail.com>
2023-09-22 18:03:39 +0000

    feat: added slip function

--- surface_crack_detection/utils/utils.py
+++ surface_crack_detection/utils/utils.py
@@ -1,9 +1,8 @@
 # bibliotecas necessárias
 import pandas as pd
 from pathlib import Path
-
-# função que gerará os dataframes 
-
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 def generate_df(image_dir, label):
 
@@ -48,3 +47,75 @@ def save_dataset(paths=["dataset/Positive", "dataset/Negative"], filename="datas
     dataset.to_csv(filename, index = False)
 
     return dataset
+
+def split_data(train_df, test_df, image_width=227,
+                image_height=227, image_channels=255.0, 
+                classes_names=['Sem fissura', 'Com fissura'], class_mode='binary', validation_split=0.2):
+    
+    """
+    Divide o dataset em treino, validação e teste
+
+    Args:
+        train_df (pd.DataFrame): dataframe com os dados das imagens de treino
+        test_df (pd.DataFrame): dataframe com os dados das imagens de teste
+        image_width (int): largura da imagem
+        image_height (int): altura da imagem
+        image_channels (int): número de canais da imagem
+        classes_names (list): lista com os nomes das classes
+        class_mode (str): modo de classificação, binário ou categórico
+        validation_split (float): proporção de imagens para validação
+
+    Returns:
+        train_data (pd.DataFrame): dataframe com os dados das imagens de treino
+        valid_data (pd.DataFrame): dataframe com os dados das imagens de validação
+        test_data (pd.DataFrame): dataframe com os dados das imagens de teste
+    """
+
+
+    image_size = (image_width, image_height)
+
+
+    train_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1./image_channels, validation_split=validation_split)
+    test_gen = tf.keras.preprocessing.image.ImageDataGenerator(
+        rescale=1./image_channels)
+
+    train_data = train_gen.flow_from_dataframe(
+        train_df,
+        x_col='Filepath',
+        y_col='Label',
+        target_size=image_size,
+        color_mode='rgb',
+        class_mode=class_mode,
+        batch_size=32,
+        shuffle=False,
+        seed=42,
+        subset="training"
+    )
+
+    valid_data = train_gen.flow_from_dataframe(
+        train_df,
+        x_col='Filepath',
+        y_col='Label',
+        target_size=image_size,
+        color_mode='rgb',
+        class_mode=class_mode,
+        batch_size=32,
+        shuffle=False,
+        seed=42,
+        subset="validation"
+    )
+
+    test_data = test_gen.flow_from_dataframe(
+        test_df,
+        x_col='Filepath',
+        y_col='Label',
+        target_size=image_size,
+        color_mode='rgb',
+        class_mode=class_mode,
+        batch_size=32,
+        shuffle=False,
+        seed=42,
+    )
+
+    return train_data, valid_data, test_data
