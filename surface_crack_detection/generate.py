aa5c69e93a6173368963e059661fc3c13947db1d
Rosana <rosanacibely@gmail.com>
2023-09-22 15:46:40 +0000

    feat: added function save_dataset

--- surface_crack_detection/generate.py
+++ surface_crack_detection/generate.py
@@ -1,15 +1,8 @@
-from utils import generate_df
+from utils import save_dataset
 from pathlib import Path
 import pandas as pd
 
-# caminho relativo das imagens
-positive_dir = Path("../dataset/Positive/")
-negative_dir = Path("../dataset/Negative/")
 
-# dataframe positivos e negativos
-positive_df = generate_df(positive_dir, label = 'POSITIVE')
-negative_df = generate_df(negative_dir, label = "NEGATIVE") 
+dataset = save_dataset(paths=["dataset/Positive/", "dataset/Negative/"], filename="dataset/dataset_final.csv")
 
-# criando um único datafrem
-all_df = pd.concat([positive_df, negative_df], axis = 0).sample(frac = 1.0, random_state = 1).reset_index(drop=True)
-print(all_df)
+print(dataset.head())
--- surface_crack_detection/utils/utils.py
+++ surface_crack_detection/utils/utils.py
@@ -1,16 +1,50 @@
 # bibliotecas necessárias
 import pandas as pd
+from pathlib import Path
 
 # função que gerará os dataframes 
+
+
 def generate_df(image_dir, label):
 
-    # diretório das imagens
-    filepaths = pd.Series(list(image_dir.glob(r'*.jpg')), name = 'Filepath').astype(str)
+    """
+    Gera um dataframe com os dados das imagens
+
+    Args:
+        image_dir (str): caminho da pasta com as imagens
+        label (str): rótulo da imagem
+
+    Returns:
+        df (pd.DataFrame): dataframe com os dados das imagens
+    """
     
-    # nome atribuído a cada imagem do diretório
-    labels = pd.Series(label, name = 'Label', index = filepaths.index)
-    
-    # criando o dataframe através da concatenação do caminho e dos labels das imagens
-    df = pd.concat([filepaths, labels], axis = 1)
+    filepaths = pd.Series(list(image_dir.glob(r'*.jpg')), name='Filepath').astype(str)
+    labels = pd.Series(label, name='Label', index=filepaths.index) 
+    df = pd.concat([filepaths, labels], axis=1)
     
     return df
+
+
+
+def save_dataset(paths=["dataset/Positive", "dataset/Negative"], filename="dataset_final.csv"):
+    """
+    Salva o dataset em um arquivo csv
+
+    Args:
+        paths (list): lista com os caminhos das pastas com as imagens
+        filename (str): nome do arquivo csv que será salvo
+
+    Returns:
+        dataset (pd.DataFrame): dataframe com os dados das imagens
+    """
+
+    positive_dir = Path(paths[0])
+    negative_dir = Path(paths[1])
+
+    positive_df = generate_df(positive_dir, label = 'POSITIVE')
+    negative_df = generate_df(negative_dir, label = "NEGATIVE") 
+
+    dataset = pd.concat([positive_df, negative_df], axis = 0).sample(frac = 1.0, random_state = 1).reset_index(drop=True)
+    dataset.to_csv(filename, index = False)
+
+    return dataset
