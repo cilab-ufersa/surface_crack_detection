f5a97db7c50b1e81d2d1e57920d17824705fdcbe
Letícia <leti.morais48@gmail.com>
2024-03-08 21:16:22 +0000

    feat: add unet_resnet50

--- surface_crack_detection/crack_segmentation/final_model.py
+++ surface_crack_detection/crack_segmentation/final_model.py
@@ -13,7 +13,7 @@ from subroutines.loss_metrics import (
     F1_score_dil,
 )
 from utils.utils import split_data
-from keras.layers import Resizing
+from keras.layers import Resizing, Concatenate
 
 dataset = pd.read_csv("dataset/dataset_final.csv")
 
@@ -21,13 +21,16 @@ train_df, test_df = train_test_split(
     dataset.sample(6000, random_state=42), train_size=0.80, random_state=42
 )
 
+preprocess_input = tf.keras.applications.resnet.preprocess_input
+
 train_data, valid_data, test_data = split_data(
-    train_df, test_df, image_width=224, image_height=224, class_mode="categorical"
+    train_df, test_df, image_width=224, image_height=224, 
+    image_channels=1.0, preprocess_input=preprocess_input
 )
 
 # modelo de segmentação unet
 unet_model = tf.keras.models.load_model(
-    "surface_crack_detection/crack_segmentation/output/checkpoints/crack_detection_3_epoch_20_F1_score_dil_0.776.h5",
+    "surface_crack_detection/crack_segmentation/output/checkpoints/crack_detection_4_epoch_87_F1_score_0.755.h5",
     custom_objects={
         "loss": Weighted_Cross_Entropy(10),
         "F1_score": F1_score,
@@ -41,30 +44,29 @@ unet_model.load_weights(
     "surface_crack_detection/crack_segmentation/output/weights/crack_detection_1_epoch_9_F1_score_dil_0.812.h5"
 )
 
-# modelo de classificação cnn
-cnn_model = tf.keras.models.load_model(
-    "surface_crack_detection/crack_segmentation/output/checkpoints/model99.h5"
+# modelo de classificação resnet
+resnet_model = tf.keras.models.load_model(
+    "surface_crack_detection/models/trained/resnet50_model.h5"
 )
 
 # desativando as camadas de treino da unet
 for layer in unet_model.layers:
     layer.trainable = False
 
-# redimensionando a unet para 96x96
-resized_unet_output = Resizing(96, 96)(unet_model.output)
+resized_unet_output = Concatenate()([unet_model.output, unet_model.output, unet_model.output])
 
-cnn_output = cnn_model(resized_unet_output)
+resnet_output = resnet_model(resized_unet_output)
 
-combined_model = tf.keras.models.Model(inputs=unet_model.input, outputs=cnn_output)
+combined_model = tf.keras.models.Model(inputs=unet_model.input, outputs=resnet_output)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
 
-combined_model.compile(optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
+combined_model.compile(optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 combined_model.summary()
 
-combined_model.fit(train_data, validation_data=valid_data, epochs=10)
+combined_model.fit(train_data, validation_data=valid_data, epochs=100)
 
 combined_model.save(
-    "surface_crack_detection/crack_segmentation/output/checkpoints/final_model_2.h5"
+    "surface_crack_detection/crack_segmentation/output/checkpoints/unet_resnet50.h5"
 )
